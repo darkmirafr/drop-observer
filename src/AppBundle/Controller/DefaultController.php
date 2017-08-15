05b8eec37aba88bc96b822933b0b2c6a4667c72d
cvilleger <villeger.c@gmail.com>
2017-08-15 02:31:56 +0200

    init

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,17 +3,18 @@
 namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends FOSRestController
 {
     public function postTweetAction(Request $request)
     {
-        // $data = json_decode($request->getContent(), true);
-
-        $logger = $this->get('logger');
-        $logger->critical($request->getContent());
-        $logger->debug($request->getContent());
+        $data = json_decode($request->getContent(), true);
+        $log = new Logger('event');
+        $log->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+        $log->addDebug('Foo', $data);
 
         // TODO Continue
     }
