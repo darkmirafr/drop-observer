46df49c95cc14617446426828f849b5afd14e2ee
cvilleger <villeger.c@gmail.com>
2017-08-19 01:29:31 +0200

    init

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
 use FOS\RestBundle\Controller\FOSRestController;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -10,13 +11,38 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends FOSRestController
 {
-    public function postTweetAction(Request $request)
+    public function getTweetsAction(Request $request)
     {
-        $data = json_decode($request->getContent(), true);
-        $log = new Logger('tweet');
+        $log = new Logger('twitter');
         $log->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
-        $log->addDebug('data', $data);
+        $consumerKey = getenv('TWITTER_CONSUMER_KEY');
+        $consumerSecret = getenv('TWITTER_CONSUMER_SECRET');
+        $accessToken = getenv('TWITTER_ACCESS_TOKEN');
+        $accessTokenSecret = getenv('TWITTER_ACCESS_TOKEN_SECRET');
 
-        return new JsonResponse;
+        if (true === $this->get('kernel')->isDebug()){
+            $consumerKey = $this->getParameter('twitter_consumer_key');
+            $consumerSecret = $this->getParameter('twitter_consumer_secret');
+            $accessToken = $this->getParameter('twitter_access_token');
+            $accessTokenSecret = $this->getParameter('twitter_access_token_secret');
+        }
+
+        $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+        $mentionsTimeline = $connection->get('statuses/mentions_timeline', ['count' => 200]);
+        $data = [];
+        foreach ($mentionsTimeline as $mentionTimeline){
+            $data[] = [
+                'id' => $mentionTimeline->id,
+                'created_at' => $mentionTimeline->created_at,
+                'text' => $mentionTimeline->text,
+                'user_id' => $mentionTimeline->user->id,
+                'user_name' => $mentionTimeline->user->name,
+                'user_screen_name' => $mentionTimeline->user->screen_name,
+            ];
+            $log->addDebug('event', $data);
+        }
+
+        return new JsonResponse($data);
     }
 }
