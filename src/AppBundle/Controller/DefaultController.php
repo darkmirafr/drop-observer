5b6fc661696adaa982ea3b1a60b22c83e0ce85c6
Christophe Villeger <cvilleger@users.noreply.github.com>
2017-10-02 22:42:00 +0200

    WIP : Feature/init industrial (#5)
    * All is fresh and new
    
    * Remove check for prod travis
    
    * Set default working template
    
    * Set nice working default home and test with it
    
    * Add phpunit and test as env test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,20 +3,13 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
 
     public function indexAction(Request $request)
     {
-        $data = '';
-        foreach(range(0,2000) as $n){
-            $data .= round(pow((sqrt(5)+1)/2, $n) / sqrt(5)) . ' ';
-        }
-
-        return new JsonResponse(['data' => $data], Response::HTTP_I_AM_A_TEAPOT);
+        return $this->render('default/index.html.twig');
     }
 }
