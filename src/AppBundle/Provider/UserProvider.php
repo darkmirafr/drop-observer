f771894ccf332e49324739bff5637eca35fc0652
Christophe Villeger <cvilleger@users.noreply.github.com>
2017-11-23 23:01:57 +0100

    [feat] Login (#8)

---
+++ src/AppBundle/Provider/UserProvider.php
@@ -0,0 +1,39 @@
+<?php
+namespace AppBundle\Provider;
+
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
+
+class UserProvider implements OAuthAwareUserProviderInterface
+{
+    /** @var EntityManagerInterface $em */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
+    {
+        $email = $response->getEmail();
+        $username = $response->getNickname();
+        $userRepository = $this->em->getRepository(User::class);
+
+        $user = $userRepository->findOneByEmail($email);
+
+        if (empty($user)){
+            $user = new User();
+            $user->setEmail($email);
+            $user->setUsername($username);
+            $this->em->persist($user);
+            $this->em->flush();
+            return $user;
+        }
+
+        return $user;
+    }
+
+}
--- web/js/init.js
+++ web/js/init.js
@@ -1,3 +1,10 @@
 $(function() {
     $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $(".dropdown-button").dropdown({ hover: false });
+    $("#form-settings").submit(function( event ) {
+        event.preventDefault();
+        $('#preloader').removeClass('hide');
+    });
 });
