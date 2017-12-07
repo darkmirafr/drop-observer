e8965bc02f45a2b88635296d7b5da9b9cbde67c7
Christophe Villeger <villeger.c@gmail.com>
2017-12-08 00:08:53 +0100

    fix travis and add phpunit

--- tests/AppBundle/Controller/HomeControllerTest.php
+++ tests/AppBundle/Controller/HomeControllerTest.php
@@ -4,15 +4,15 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class HomeControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testNavbar()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('obServer', $crawler->filter('#logo-container')->text());
     }
 }
