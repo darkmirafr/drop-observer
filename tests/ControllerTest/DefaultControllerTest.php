3be4091254275e34b1451bf229081255881b1ae6
Christophe Villeger <villeger.c@gmail.com>
2018-02-16 01:36:57 +0100

    Fix tests

--- tests/ControllerTest/DefaultControllerTest.php
+++ tests/ControllerTest/DefaultControllerTest.php
@@ -6,11 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testShowPost()
+    public function testShowHome()
     {
         $client = static::createClient();
 
         $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testShowHelp()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/about');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testShowLogin()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/login');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
