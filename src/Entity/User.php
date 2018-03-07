120d0561f1316d5a043c78b0c61a311329e50a68
Christophe Villeger <villeger.c@gmail.com>
2018-03-07 03:44:48 +0100

    fix user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,18 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use HWI\Bundle\OAuthBundle\Connect\AccountConnectorInterface;
-use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
-use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User implements UserInterface ,UserProviderInterface, AccountConnectorInterface, OAuthAwareUserProviderInterface
+class User implements UserInterface
 {
     /**
      * @var int
@@ -70,72 +64,6 @@ class User implements UserInterface ,UserProviderInterface, AccountConnectorInte
     }
 
     public function getPassword(): void
-    {
-    }
-
-    /**
-     * Loads the user for the given username.
-     *
-     * This method must throw UsernameNotFoundException if the user is not
-     * found.
-     *
-     * @param string $username The username
-     *
-     * @return UserInterface
-     *
-     * @throws UsernameNotFoundException if the user is not found
-     */
-    public function loadUserByUsername($username)
-    {
-    }
-
-    /**
-     * Refreshes the user.
-     *
-     * It is up to the implementation to decide if the user data should be
-     * totally reloaded (e.g. from the database), or if the UserInterface
-     * object can just be merged into some internal array of users / identity
-     * map.
-     *
-     * @return UserInterface
-     *
-     * @throws UnsupportedUserException if the user is not supported
-     */
-    public function refreshUser(UserInterface $user)
-    {
-    }
-
-    /**
-     * Whether this provider supports the given user class.
-     *
-     * @param string $class
-     *
-     * @return bool
-     */
-    public function supportsClass($class)
-    {
-    }
-
-    /**
-     * Connects the response to the user object.
-     *
-     * @param UserInterface $user The user object
-     * @param UserResponseInterface $response The oauth response
-     */
-    public function connect(UserInterface $user, UserResponseInterface $response)
-    {
-    }
-
-    /**
-     * Loads the user by a given UserResponseInterface object.
-     *
-     * @param UserResponseInterface $response
-     *
-     * @return UserInterface
-     *
-     * @throws UsernameNotFoundException if the user is not found
-     */
-    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
     }
 }
