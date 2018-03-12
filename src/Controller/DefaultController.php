d96bc2a5473b0cd3ebe51cc1c7fcfbe307165f35
Christophe Villeger <villeger.c@gmail.com>
2018-03-12 22:46:10 +0100

    Codestyle

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
+use App\Form\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
