cb0d68ad13aa0c3ce7c83e21c387db79610ad2e1
Christophe Villeger <villeger.c@gmail.com>
2018-03-14 22:38:28 +0100

    Add login form init

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\Type\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
@@ -12,13 +14,20 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig');
     }
 
-    public function login()
+    public function login(Request $request, UserRepository $userRepository)
     {
-
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            return $this->redirectToRoute('admin');
+            $user = $userRepository->findOneBy(['email' => $form->getData()->getEmail()]);
+            if (null === $user){
+                $this->addFlash('warning', 'Wrong email or password, try again.');
+                return $this->render('default/login.html.twig', [
+                    'form' => $form->createView()
+                ]);
+            }
+            $this->addFlash('success', 'Email found.');
         }
 
         return $this->render('default/login.html.twig', [
