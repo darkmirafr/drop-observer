3913debbc8acd8a349cdcbfaf8287a7aca382d55
Christophe Villeger <villeger.c@gmail.com>
2018-03-27 01:52:12 +0200

    Add logout, remove handleRequest form, set last_email in form and update

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,20 +5,18 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
-    public function login(Request $request, AuthenticationUtils $authenticationUtils)
+    public function login(AuthenticationUtils $authenticationUtils)
     {
         $user = new User;
         $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
 
         return $this->render('security/login.html.twig', [
             'form' => $form->createView(),
-            'last_username' => $authenticationUtils->getLastUsername(),
+            'last_email' => $authenticationUtils->getLastUsername(),
             'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
     }
