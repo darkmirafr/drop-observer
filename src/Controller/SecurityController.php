823efbe8579520940a7831408b9510075d253504
Christophe Villeger <villeger.c@gmail.com>
2018-04-11 18:13:02 +0200

    Add login-check route and HTTP_HOST for phpunit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,10 +6,17 @@ use App\Entity\User;
 use App\Form\Type\UserType;
 use App\Service\GoogleService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
+    /**
+     * Handle Classic form and Google
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function login(AuthenticationUtils $authenticationUtils)
     {
         $user = new User;
@@ -20,5 +27,21 @@ class SecurityController extends Controller
             'error' => $authenticationUtils->getLastAuthenticationError(),
             'google_redirect_uri' => $this->get(GoogleService::class)->getRedirectUri(),
         ]);
+    }
+
+    /**
+     * Handle Google Login check
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function loginCheck(Request $request)
+    {
+        if (null !== $request->get('error')){
+            return $this->redirectToRoute('login');
+        }
+
+        $this->get(GoogleService::class)->authUser($request->get('code'));
+        return $this->redirectToRoute('index');
     }
 }
--- src/Service/GoogleService.php
+++ src/Service/GoogleService.php
@@ -1,15 +1,29 @@
 <?php
 namespace App\Service;
 
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 class GoogleService
 {
+    private $googleClient;
+    private $session;
+
+    public function __construct(\Google_Client $googleClient, SessionInterface $session)
+    {
+        $this->googleClient = $googleClient;
+        $this->googleClient->setIncludeGrantedScopes(true);
+        $this->googleClient->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/login-check');
+        $this->googleClient->useApplicationDefaultCredentials();
+        $this->session = $session;
+    }
+
     public function getRedirectUri(): string
     {
-        $client = new \Google_Client();
-        $client->setIncludeGrantedScopes(true);
-        $client->addScope(\Google_Service_Logging::LOGGING_READ);
-        $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/check-login');
+        return $this->googleClient->createAuthUrl(\Google_Service_Oauth2::USERINFO_EMAIL);
+    }
 
-        return $client->createAuthUrl();
+    public function authUser(string $authCode)
+    {
+        return $this->googleClient->fetchAccessTokenWithAuthCode($authCode);
     }
 }
