d5a607c7e4d8eee7e2bf17593cdff836eb30c4de
Christophe Villeger <villeger.c@gmail.com>
2018-04-25 00:36:29 +0200

    code style

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,19 @@
 $(function() {
     $('#twitter-launch-btn').click(function() {
-        $(this).attr('disabled', 'disabled');
-        var progress = $('.progress').children();
+        $(this).attr("disabled", "disabled");
+        var progress = $(".progress").children();
         progress.removeClass('d-none');
 
-        var jqxhr = $.get('/twitter', function( data ) {
-            progress.width('100%').html('100%');
-            progress.fadeOut('slow');
+        var jqxhr = $.get("/twitter", function( data ) {
+            progress.width("100%").html("100%");
+            progress.fadeOut("slow");
             data.forEach(function(element) {
-                $('#tweet-list').append('<li class="list-group-item">' + element.text + '</li>');
+                $('#tweet-list').append('<li class="list-group-item">' + element.text + "</li>");
             });
         }).fail(function() {
-            progress.removeClass('bg-success');
-            progress.addClass('bg-danger');
-            progress.width('100%').html('Error');
+            progress.removeClass("bg-success");
+            progress.addClass("bg-danger");
+            progress.width("100%").html("Error");
         });
 
     });
